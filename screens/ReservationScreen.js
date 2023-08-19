4ec7dc090a7a8d74de0dfa749833b08ee187ea15
JimPaul <jpaulkaito@gmail.com>
2023-08-18 18:26:29 -0700

    Modal2

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -1,5 +1,13 @@
 import { useState } from 'react';
-import { Text, View, ScrollView, StyleSheet, Switch, Button, Modal } from 'react-native';
+import {
+    Text,
+    View,
+    ScrollView,
+    StyleSheet,
+    Switch,
+    Button,
+    Modal
+} from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
@@ -21,10 +29,6 @@ const ReservationScreen = () => {
         console.log('hikeIn:', hikeIn);
         console.log('date:', date);
         setShowModal(!showModal);
-        setCampers(1);
-        setHikeIn(false);
-        setDate(new Date());
-        setShowCalendar(false);
     };
 
     const resetForm = () => {
@@ -32,7 +36,7 @@ const ReservationScreen = () => {
         setHikeIn(false);
         setDate(new Date());
         setShowCalendar(false);
-    }
+    };
 
     return (
         <ScrollView>
@@ -92,30 +96,30 @@ const ReservationScreen = () => {
                 visible={showModal}
                 onRequestClose={() => setShowModal(!showModal)}
             >
-            <View style={styles.modal}>
-                <Text style={styles.modalTitle}>
-                    Search Campsite Reservations
-                </Text>
-                <Text style={styles.modalText}>
-                    Number of Campers: {campers}
-                </Text>
-                <Text style={styles.modalText}>
-                    Hike-In?: {hikeIn ? 'Yes' : 'No'}
-                </Text>
-                <Text style={styles.modalText}>
-                    Date: {date.toLocaleDateString('en-US')}
-                </Text>
-                <Button
-                    onPress={() => {
-                        setShowModal(!showModal);
-                        resetForm();
-                    }}
-                    color='#5637DD'
-                    title='Close'
-                />
-            </View>
-        </Modal>
-        </ScrollView >
+                <View style={styles.modal}>
+                    <Text style={styles.modalTitle}>
+                        Search Campsite Reservations
+                    </Text>
+                    <Text style={styles.modalText}>
+                        Number of Campers: {campers}
+                    </Text>
+                    <Text style={styles.modalText}>
+                        Hike-In?: {hikeIn ? 'Yes' : 'No'}
+                    </Text>
+                    <Text style={styles.modalText}>
+                        Date: {date.toLocaleDateString('en-US')}
+                    </Text>
+                    <Button
+                        onPress={() => {
+                            setShowModal(!showModal);
+                            resetForm();
+                        }}
+                        color='#5637DD'
+                        title='Close'
+                    />
+                </View>
+            </Modal>
+        </ScrollView>
     );
 };
 
